5c8bd8031a0caeb3186cc1cd6a8b0fd8a254d89d
YamazakiSunao <49880780+YamazakiSunao@users.noreply.github.com>
2020-03-02 00:51:21 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const selectElement = function (element) {
+    return document.querySelector(element);
+};
+
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
+
+menuToggler.addEventListener('click', function(){
+    body.classList.toggle('open');
+});
+
+//scrollReveal
+
+ScrollReveal().reveal('.animate-left', {
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300
+});
+
+ScrollReveal().reveal('.animate-right', {
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+
+ScrollReveal().reveal('.animate-top', {
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+
+ScrollReveal().reveal('.animate-bottom', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
